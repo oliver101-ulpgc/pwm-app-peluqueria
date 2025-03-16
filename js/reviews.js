62778c3c5694e0518b495de01c0269d879ff0b00
Oliver Cabrera <oliver.cabrera101@alu.ulpgc.es>
2025-03-16 16:07:13 +0000

    Cargar reviews desde reviews.json

--- js/reviews.js
+++ js/reviews.js
@@ -4,11 +4,11 @@ async function addReviews(data) {
     [...data.data].forEach((review) => {
         const reviewElement = reviewTemplate.cloneNode(true);
 
-        const username = reviewElement.querySelector('h3');
+        const username = reviewElement.querySelector('.review-username');
         if (username) {
             username.textContent = review.client.username;
         }
-        const description = reviewElement.querySelector('p');
+        const description = reviewElement.querySelector('.review-description');
         if (description) {
             description.textContent = review.text;
         }
@@ -19,6 +19,10 @@ async function addReviews(data) {
         // TODO: add user avatar and stars
         const stars = reviewElement.querySelector('.star');
         if (stars) {
+            // TODO
+        }
+        const avatar = reviewElement.querySelector('.review-avatar');
+        if (avatar) {
             // TODO
         }
 

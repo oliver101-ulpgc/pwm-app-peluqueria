ce43a927b74f6de1afe94a598cb4818278921304
Entrar <raulsanchez1478@gmail.com>
2025-03-18 20:32:53 +0000

    Cambios en el menú js

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,9 @@
 function toggleMenu() {
     const menu = document.getElementById("menuContainer");
 
-    // Si el menú no ha sido mostrado antes, lo mostramos
-    if (menu.style.display === "none") {
-        menu.style.display = "flex";  // Lo mostramos con flex
-        setTimeout(() => {  // Usamos un pequeño retraso para asegurar la animación
-            menu.classList.add("active");
-        }, 10);  // Añadimos un pequeño retraso antes de activar la animación
-    } else {
+    if (menu.classList.contains("active")) {
         menu.classList.remove("active");
-        setTimeout(() => {
-            menu.style.display = "none";  // Ocultamos el menú después de la animación
-        }, 300);  // El tiempo de espera debe coincidir con la duración de la animación
+    } else {
+        menu.classList.add("active");
     }
 }

ba3515f5d5ef481802af41d7bd28714c8ebb43a0
Oliver Cabrera <oliver.cabrera101@alu.ulpgc.es>
2025-03-24 16:15:29 +0000

    Separar en gallery-card.html del portfolio.js
    No trackear clicks en portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,47 +1,30 @@
-// Manejar clics y actualizar contador
-function trackClicks(itemId, type) {
-    const storageKey = `${type}_clicks`;
-    let clicks = JSON.parse(localStorage.getItem(storageKey)) || {};
-    clicks[itemId] = (clicks[itemId] || 0) + 1;
-    localStorage.setItem(storageKey, JSON.stringify(clicks));
-}
-
-// home.js actualizado
 document.addEventListener('DOMContentLoaded', async () => {
     await loadCommonTemplates();
 
     const data = await fetchData('../../../data/portfolio.json');
     if (!data) return;
 
-    // Obtener y mezclar datos
-    const allItems = data.data.map(item => ({
-        ...item,
-        clicks: JSON.parse(localStorage.getItem(`${item.type}_clicks`))?.[item.id] || 0
-    }));
-
-    // Ordenar por popularidad y limitar a los 6 más clickeados
-    const sortedItems = [...allItems].sort((a, b) => b.clicks - a.clicks);
-
     // Filtrar por tipo
-    const primaryItems = sortedItems.filter(item => item.type === "hombre");
-    const secondaryItems = sortedItems.filter(item => item.type === "mujer");
+    const primaryItems = data.data.filter(item => item.type === "hombre");
+    const secondaryItems = data.data.filter(item => item.type === "mujer");
 
-    // Función para generar tarjetas
-    const generateCards = (items, containerId) => {
+    // Generar tarjetas
+    const generateCards = async (items, containerId) => {
+        const cardTemplate = await fetchTemplate('./gallery-card.html');
         const container = document.getElementById(containerId);
         if (!container) return;
-        container.innerHTML = items.map(item => `
-                <div class="card">
-                <img src="${item.image}" alt="${item.title}">
-                    <div class="card-body">
-                    <h3>${item.name}</h3>
-                    </div>
-                </div>
-        `).join('');
+        items.forEach((item) => {
+            const card = cardTemplate.cloneNode(true);
+            card.querySelector('h3').textContent = item.name;
+            const cardImg = card.querySelector('.gallery-image');
+            cardImg.src = item.image;
+            cardImg.alt = item.name;
+            container.appendChild(card);
+        })
     };
 
     // Renderizar tarjetas en sus respectivas secciones
-    generateCards(primaryItems, 'main_section');
-    generateCards(secondaryItems, 'secondary_section');
+    await generateCards(primaryItems, 'main_section');
+    await generateCards(secondaryItems, 'secondary_section');
 });
 

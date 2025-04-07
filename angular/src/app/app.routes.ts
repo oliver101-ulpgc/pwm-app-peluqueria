0e544030e8304491f3cb7b1611ac714330eff0c7
Lucia <nancimotasguedes@gmail.com>
2025-04-07 15:40:26 +0100

    booking updated

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import {HomeComponent} from './pages/home/home.component';
 import {PortfolioComponent} from './pages/portfolio/portfolio.component';
 import {DetailsComponent} from './pages/details/details.component';
 import {ReviewsComponent} from './pages/reviews/reviews.component';
+import {BookingComponent} from './pages/booking/booking.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'portfolio', component: PortfolioComponent},
   { path: 'details', component: DetailsComponent},
   { path: 'reviews', component: ReviewsComponent},
+  { path: 'booking', component: BookingComponent},
 ];
 
 @NgModule({

ce9a3584db61fb31483d8cc17f08a54b9f371e87
Entrar <raulsanchez1478@gmail.com>
2025-04-29 20:33:47 +0100

    appointment component; get appointment

--- angular/src/app/pages/appointments/appointments.component.ts
+++ angular/src/app/pages/appointments/appointments.component.ts
@@ -1,6 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {CommonPageComponent} from '../../components/common_page/common_page';
+import {AppoinmentsService} from '../../services/appoinments.service';
+import {Appoinment, Portfolio} from '../../models/interfaces.model';
 
 @Component({
   selector: 'appointments-component',
@@ -10,6 +12,17 @@ import {CommonPageComponent} from '../../components/common_page/common_page';
   styleUrl: './appointments.component.css'
 })
 
-export class AppointmentsComponent {
+export class AppointmentsComponent implements OnInit{
 
+  appoinments: Appoinment[] = [];
+
+  constructor(private appointmentService: AppoinmentsService) {}
+
+  ngOnInit(): void {
+    this.appointmentService.getAppointments().subscribe({
+      next: (appointment: Appoinment[]) => {
+        this.appoinments = appointment;
+      },
+    });
+  }
 }
--- angular/src/app/services/appoinments.service.ts
+++ angular/src/app/services/appoinments.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {addDoc, collection, Firestore} from '@angular/fire/firestore';
+import {addDoc, collection, collectionData, Firestore} from '@angular/fire/firestore';
 import {Appoinment} from '../models/interfaces.model';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,13 @@ export class AppoinmentsService {
 
   constructor(private firestore: Firestore) { }
 
-  addAppoinment(appoinment: Appoinment) {
-    const appoinmentRef = collection(this.firestore, 'appointments');
-    return addDoc(appoinmentRef, appoinment);
+  addAppointment(appoinment: Appoinment) {
+    const appointmentRef = collection(this.firestore, 'appointments');
+    return addDoc(appointmentRef, appoinment);
+  }
+
+  getAppointments(){
+    const appointmentRef = collection(this.firestore, 'appointments');
+    return collectionData(appointmentRef, {idField: 'id'}) as Observable<Appoinment[]>
   }
 }

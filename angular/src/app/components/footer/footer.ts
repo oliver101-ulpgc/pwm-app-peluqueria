49b044519872becb1846ff961ad46b7bed1f70c3
Oliver Cabrera <oliver.cabrera101@alu.ulpgc.es>
2025-04-30 00:03:41 +0100

    Fix typo and fix links

--- angular/src/app/components/footer/footer.ts
+++ angular/src/app/components/footer/footer.ts
@@ -1,12 +1,13 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
-    selector: 'footer-component',
-    styleUrls: ['footer.css', '../common_page/common.css'],
-    templateUrl: './footer.html',
-    standalone: true,
-    imports: [CommonModule]
+  selector: 'footer-component',
+  styleUrls: ['footer.css', '../common_page/common.css'],
+  templateUrl: './footer.html',
+  standalone: true,
+  imports: [CommonModule, RouterLink]
 })
 
 export class FooterComponent {}

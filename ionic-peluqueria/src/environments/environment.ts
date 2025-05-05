f30427d2f4f8b8d83fab9710f80c8a0391ad7b84
raulss <raulsanchez@gmail.com>
2025-05-05 16:48:19 +0100

    envorionment

--- ionic-peluqueria/src/environments/environment.ts
+++ ionic-peluqueria/src/environments/environment.ts
@@ -2,9 +2,31 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+/*
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 export const environment = {
-  production: false
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCD2NV2JOPtFDCkveRZGjBWz2lVQM6IPX8",
+    authDomain: "pwm-peluqeria.firebaseapp.com",
+    projectId: "pwm-peluqeria",
+    storageBucket: "pwm-peluqeria.firebasestorage.app",
+    messagingSenderId: "932508723937",
+    appId: "1:932508723937:web:e4329f7586d913d233e5a0",
+    measurementId: "G-7Z9L7VRQJ3"
+  };
+
+// Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 };
+*/
 
 /*
  * For easier debugging in development mode, you can import the following file

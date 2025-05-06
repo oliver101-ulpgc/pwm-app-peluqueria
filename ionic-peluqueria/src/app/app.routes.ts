374e18d0444ee8dd65834a29ea810ba6ec4db541
Entrar <raulsanchez1478@gmail.com>
2025-05-06 20:06:47 +0100

    Log in

--- ionic-peluqueria/src/app/app.routes.ts
+++ ionic-peluqueria/src/app/app.routes.ts
@@ -3,11 +3,20 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: 'log-in',
+    loadComponent: () => import('./pages/log-in/log-in.page').then((m) => m.LogInPage),
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'log-in',
+    loadComponent: () => import('./pages/log-in/log-in.page').then( m => m.LogInPage)
+  },
+
 ];
---
+++ ionic-peluqueria/src/app/pages/log-in/log-in.page.ts
@@ -0,0 +1,43 @@
+import {Component, inject, OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {Router, RouterLink} from "@angular/router";
+import {IonicModule} from "@ionic/angular";
+
+@Component({
+  selector: 'app-log-in',
+  templateUrl: './log-in.page.html',
+  styleUrls: ['./log-in.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterLink, IonicModule, ReactiveFormsModule]
+})
+export class LogInPage{
+
+  private formBuilder = inject(FormBuilder);
+
+  constructor(private router: Router) {}
+
+  form = this.formBuilder.group({
+    email: this.formBuilder.control('', Validators.required),
+    password: this.formBuilder.control('', Validators.required),
+  })
+
+  async submit() {
+    if (this.form.invalid) {
+      return
+    }
+
+    try {
+      const email = this.form.get('email')?.value;
+      const password = this.form.get('password')?.value;
+
+      if (!email || !password) {
+        return;
+      }
+
+      await this.router.navigate(['']);
+    } catch (error) {
+    }
+  };
+}

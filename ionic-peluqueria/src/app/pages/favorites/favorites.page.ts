cdabd800a04eb5135f7fb42e8bccb11a97273e75
Oliver Cabrera <oliver.cabrera101@alu.ulpgc.es>
2025-05-25 21:56:32 +0100

    Use service-item component in favorites.page

--- ionic-peluqueria/src/app/pages/favorites/favorites.page.ts
+++ ionic-peluqueria/src/app/pages/favorites/favorites.page.ts
@@ -11,23 +11,24 @@ import {
   IonToolbar, IonBackButton
 } from '@ionic/angular/standalone';
 import {Service} from "../../models/service.model";
-import { RouterModule } from '@angular/router';
+import {Router, RouterModule} from '@angular/router';
 import {NgForOf} from "@angular/common";
 import {AppServicesService} from "../../services/app-services.service";
 import {Subscription} from "rxjs";
+import {ServiceItemComponent} from "../../components/service/service-item/service-item.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'favorites.page.html',
   styleUrls: ['favorites.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, NgForOf, IonList, IonAvatar, IonItem, IonThumbnail, IonLabel, RouterModule, IonBackButton],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, NgForOf, IonList, IonAvatar, IonItem, IonThumbnail, IonLabel, RouterModule, IonBackButton, ServiceItemComponent],
   standalone: true
 })
 export class FavoritesPage implements OnInit, OnDestroy {
   services: Service[] = [];
   private servicesSubscription?: Subscription;
 
-  constructor(private appServices: AppServicesService) {}
+  constructor(private router: Router, private appServices: AppServicesService) {}
 
   ngOnInit(): void {
     this.servicesSubscription = this.appServices.getServices().subscribe(
@@ -37,5 +38,9 @@ export class FavoritesPage implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.servicesSubscription?.unsubscribe();
+  }
+
+  goToDetails(service: Service) {
+    this.router.navigate(['/service-details'], {state: {service}}).then();
   }
 }
